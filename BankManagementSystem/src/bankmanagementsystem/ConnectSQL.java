a3d1b59596ef9cf348ee82d13c39f5ebdec65475
unknown <harshal.ad@somaiya.edu>
2018-09-14 23:53:58 +0530

    First Commit

---
+++ BankManagementSystem/src/bankmanagementsystem/ConnectSQL.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bankmanagementsystem;
+
+/**
+ *
+ * @author Harshal
+ */
+import java.sql.*;
+import javax.swing.*;
+
+public class ConnectSQL {
+    Connection conn;
+    
+    public static Connection ConnectDB(){
+        try{
+            Class.forName("org.sqlite.JDBC");
+            Connection conn=DriverManager.getConnection("jdbc:sqlite:C:\\Users\\Harshal\\Documents\\NetBeansProjects\\BankManagementSystem\\SomaiyaBank.sqlite");
+            return conn;
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null,e);
+            return null;
+        }
+    }
+}
